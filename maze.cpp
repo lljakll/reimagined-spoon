28b99798f7d516330517241a1d7b411f7481416c
lljakll <jakadair@gmail.com>
2018-10-28 10:06:15 +0430

    Modifications
    Modified Cell class to implement a 2D array for each cell (3x3).  Default constructors creates a 3x3 with '.'.  Overloaded creates a 3x3 with appropriate id in element 1,1.  Added a PrintArray() function that calls a ReturnCellValue from the Cell object defined inthe function.  This is the only way to use cout and should be removed if that ceases to be a requiremnt in the future.  There is quite a high cost for this function.

--- maze.cpp
+++ maze.cpp
@@ -7,37 +7,87 @@ using namespace std;
 
 class Cell {
 public:
+	// Default constructor that creates a blank cell (3x3 grid of '.')
 	Cell(){
-		cellLocX, cellLocY = 0;
+		for(int i = 0; i<3; i++){
+			for(int j = 0; j<3; j++){
+				content[i][j] = '.';
+			}
+		}
 		isAWall,isStart,isFinish,isBarrier,hasBeenVisited = false;
+		cellLocX, cellLocY = 0;
 	}
 	
-	Cell(int locationX, int locationY){
-		cellLocX = locationX;
-		cellLocY = locationY;
+	// overloaded constructor that accepts a char to add to cell ID (index 1,1)
+	Cell(char cellType){
+		for(int i = 0; i<3; i++){
+			for(int j = 0; j<3; j++){
+				// if a wall, 'X' and flag, otherwise '.'
+				if (cellType == 'X' || cellType == 'x') { content[i][j] = 'X'; isAWall = true;}
+				else { content[i][j] = '.';	}
+			}
+		}
+		if (cellType == 'S' || cellType == 's'){ content[1][1] = 'S'; isStart = true;}
+		else if(cellType == 'F' || cellType == 'f'){ content[1][1] = 'F'; isFinish = true;}
+
 		isAWall,isStart,isFinish,isBarrier,hasBeenVisited = false;
+		cellLocX, cellLocY = 0;
 	}
-//	void visitCell();
-	void UpdateCellLoc(int x, int y){
-		cellLocX = x;
-		cellLocY = y;
+
+	void UpdateCellType(char cellType){
+		for(int i = 0; i<3; i++){
+			for(int j = 0; j<3; j++){
+				// if a wall, 'X' and flag, otherwise '.'
+				if (cellType == 'X' || cellType == 'x') { content[i][j] = 'X'; isAWall = true;}
+				else { content[i][j] = '.';	}
+			}
+		}
+		if (cellType == 'S' || cellType == 's'){ content[1][1] = 'S'; isStart = true;}
+		else if(cellType == 'F' || cellType == 'f'){ content[1][1] = 'F'; isFinish = true;}
+
+		isAWall,isStart,isFinish,isBarrier,hasBeenVisited = false;
+		cellLocX, cellLocY = 0;
 	}
+	bool UpdateCellHasBeenVisited(){
+		return true;
+	}
+	char ReturnCellValue(int row, int col){
+		return content[row][col];
+	}
+
 protected:
-	int cellLocX, cellLocY;
+	char content[3][3];
+	int cellLocX,cellLocY;
 	bool isAWall, isStart, isFinish, isBarrier, hasBeenVisited;
 };
 
-
-int main() {
-	// Create the array of objects, update their x,y loc
-	Cell maze[12][12];
-	
-	for (int row = 0; row < 12; row++){
-		for (int col = 0; col < 12; col++){
-			maze[row,col]->UpdateCellLoc(row,col);
+// Print the array of Cell Objects to STD_OUT
+// Need to work on passing 2D array by ref
+// So we dont have to define the size of the array here
+void PrintArray(Cell array[9][9], int size){
+	for (int i = 0; i < size; i++){
+		for (int m = 0; m < 3; m++){
+			for (int j = 0; j < size; j++){
+				for (int n = 0; n < 3; n++){
+					cout << array[i][j].ReturnCellValue(m,n) << " ";
+				}
+			}
+			cout << endl;
 		}
 	}
-	
+}
+
+int main() {
+	// Create the array of objects
+	const int ARR_SZ = 9;
+	Cell maze[ARR_SZ][ARR_SZ];
+
+	PrintArray(maze, ARR_SZ);
+	cin.get();
+	return 0;
+}
+
+
 	// Read from the file
 	
 	// process the file and update the array of objects with
@@ -46,6 +96,4 @@ int main() {
 	// Display the maze
 
 	
-	cin.get();
-	return 0;
-}
+
