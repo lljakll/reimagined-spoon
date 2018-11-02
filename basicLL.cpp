909729f563bf2602aa98c0e39e801d447a2605d0
lljakll <jakadair@gmail.com>
2018-11-02 06:14:18 +0430

    Progress 50%

--- basicLL.cpp
+++ basicLL.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-Node class Node{
+class Node{
 public:
     Node(){
         next = NULL;
     }
-    Node(string passedData, Node *ptr){
+    Node(string passedData, Node *ptr = 0){
         data = passedData;
         next = ptr;
     }
@@ -21,21 +21,59 @@ public:
 
 class LinkedList{
 public:
-    LinkedList{
+    LinkedList(){
         head = 0;
         tail = 0;
     }
-    void addNodeAfterIthNode(int ithNode){
-        
+
+    // Page 79
+    void addNodeToHead(string passedData){
+        head = new Node(passedData, head);
+        if (tail ==0)
+            tail = head;
     }
-    void deleteNode()
-    void printNode()
-    bool listContains(string searchStr)
-    Node searchList(string searchStr)
-    void sortList()
-    void copyList()
-    void destructivelyAppendList()
-    void reverseList()
+    
+    // Page 79
+    void addNodeToTail(string passedData){
+        if (tail !=0){
+            tail->next = new Node(passedData);
+            tail = tail->next;
+        }
+        else   
+            head = tail = new Node(passedData);
+    }
+
+ /*   void addNodeAfterIthNode(int ithNode, string passedData){
+        // check to make sure there are nodes to insert this 
+        // node after...
+        if (tail != 0 && head != 0){
+            // set the temp pointer to the ith nodes's address
+            for (int ii = 0; ii < ithNode; II++){
+                temp->next = 
+            }
+        }
+
+    }*/
+
+//    void deleteIthNode(int ithNode){
+//    void printNode()
+//    bool listContains(string searchStr)
+//    Node searchList(string searchStr)
+//    void sortList()
+//    void copyList()
+//    void destructivelyAppendList()
+//    void reverseList()
+
 private:
     Node *head, *tail;
 };
+
+int main(){
+    LinkedList myList;
+    myList.addNodeToHead("jackie");
+
+    string wait;
+    cin >> wait;
+    return 0;
+
+}
