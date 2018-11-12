1b3c4ed310911ba9707591c0857c2b94c263fc3d
lljakll <jakadair@gmail.com>
2018-11-12 13:17:55 +0430

    Week-3 Additions
    modified maze for week 3 assignment.  Added a depth search (stack) solution algorithm.

--- maze.cpp
+++ maze.cpp
@@ -1,31 +1,199 @@
-#include <fstream>
+// Jackie Adair
+// CST-201
+// Week 3 Assignment
+// This is my own work
+//
+
+#include <cstdlib>
 #include <iostream>
+#include <stack>
+#include <fstream>
+#include <vector>
 
 // Node declaration/definition
-class DLNode{
+class DLNode {
 public:
-	DLNode(){
+	DLNode() {
 		prev = NULL;
 		next = NULL;
 	}
 
-	DLNode(char passed, DLNode *prevPtr = 0, DLNode *nextPtr = 0){
-		if(passed != 'X'){
+	DLNode(char passed, DLNode *prevPtr = 0, DLNode *nextPtr = 0) {
+		if (passed != 'X') {
 			cell[1][1] = passed;
 		}
-		else{
-			for(int i = 0; i<3;i++)
-				for(int j = 0;j<3;j++)
+		else {
+			for (int i = 0; i < 3; i++)
+				for (int j = 0; j < 3; j++)
 					cell[i][j] = 'X';
 		}
 		prev = prevPtr;
 		next = nextPtr;
 	}
+
+
 	DLNode *prev;
 	DLNode *next;
-	char cell[3][3] = { {'.','.','.'}, {'.','.','.'},{'.','.','.'} };
+	DLNode *mzUp;
+	DLNode *mzDown;
+	DLNode *mzLeft;
+	DLNode *mzRight;
+
+	char cell[3][3] = { {' ',' ',' '}, {' ',' ',' '},{' ',' ',' '} };
 	int nodeX, nodeY;
+	bool visited, backTrack;
+	bool up = true;
+	bool right = true;
+	bool down = true;
+	bool left = true;
+	int mzCellNum = 0;
 };
+
+
+class Stack {
+public:
+
+	// Clear the stack
+	void Clear() {
+		for (int i = this->Size(); i >= 0; i--)
+			values[i] = {};
+	}
+
+	// Check for an empty stack
+	bool IsEmpty() {
+		if (values[0] == nullptr)
+			return true;
+		return false;
+	}
+
+	// Push to the stack
+	void Push(DLNode *element) {
+		if (values[0] != nullptr) {
+			for (int i = this->Size(); i >= 0; i--) {
+				values[i] = values[i - 1];
+			}
+		}
+		values[0] = element;
+	}
+
+	// Pop from the stack
+	void Pop() {
+		for (int i = 0; i < 20; i++) {
+			values[i] = values[i + 1];
+		}
+		values[20] = {};
+	}
+
+	// Return the top value
+	DLNode Top() {
+		if (values[0]) return *values[0];
+		else return 0;
+	}
+
+	// Return the size of the stack
+	int Size() {
+		if (!values) {
+			return 0;
+		}
+		else {
+			int arraySize = 0;
+			for (int i = 0; i < 20; i++) {
+				if (values[i]) arraySize++;
+			}
+			return arraySize;
+		}
+	}
+
+	// Output the stack
+	void PrintStack() {
+		std::cout << std::endl;
+		for (int i = 0; i < this->Size(); i++) {
+			std::cout << "  - " << values[i] << std::endl;
+		}
+		std::cout << std::endl << " -> Stack Size: " << this->Size() << std::endl;
+		std::cout << " ____________________" << std::endl;
+		std::cout << std::endl;
+	}
+
+
+	DLNode *values[20];
+
+};
+
+template <class T>
+class Queue {
+public:
+
+	// Clear the queue
+	void Clear() {
+		for (int i = this->Size(); i >= 0; i--)
+			values[i] = {};
+	}
+
+	// Check for an empty queue
+	bool IsEmpty() {
+		if (values[0])
+			return false;
+		return true;
+	}
+
+	// Queue an element to the queue
+	void QueueItem(T element) {
+		if (values[0]) {
+			values[this->Size()] = element;
+		}
+		else {
+			values[0] = element;
+		}
+	}
+
+	// Dequeue from the queue
+	T Dequeue() {
+		T frontElement = values[0];
+		for (int i = 0; i < 20; i++) {
+			values[i] = values[i + 1];
+		}
+		values[20] = {};
+		return frontElement;
+	}
+
+	// Return the top value
+	T Front() {
+		if (values[0]) return values[0];
+		else return 0;
+	}
+
+	// Return the size of the queue
+	int Size() {
+		if (!values) {
+			return 0;
+		}
+		else {
+			int arraySize = 0;
+			for (int i = 0; i < 20; i++) {
+				if (values[i]) arraySize++;
+			}
+			return arraySize;
+		}
+	}
+
+	// Output the queue
+	void PrintQueue() {
+		std::cout << std::endl;
+		for (int i = 0; i < this->Size(); i++) {
+			std::cout << "  - " << values[i] << std::endl;
+		}
+		std::cout << std::endl << " -> Queue Size: " << this->Size() << std::endl;
+		std::cout << " ____________________" << std::endl;
+		std::cout << std::endl;
+	}
+
+private:
+	T values[50] = {};
+
+};
+
+
 
 // The list that holds the maze data
 class DoublyLinkedList {
@@ -34,7 +202,7 @@ public:
 		head = 0;
 		tail = 0;
 	}
-	DoublyLinkedList(int row, int col){
+	DoublyLinkedList(int row, int col) {
 		head = 0;
 		tail = 0;
 		sizeRow = row;
@@ -42,7 +210,7 @@ public:
 	}
 
 	// Adds a node to the list.
-	void addDLNode(char data, int x, int y) {
+	void addDLNode(char data, int x, int y, int cellNum) {
 		// If there are nodes, point at the end
 		if (tail != 0) {
 			DLNode *temp = tail;
@@ -55,6 +223,8 @@ public:
 			// update x,y of the new node
 			tail->nodeX = x;
 			tail->nodeY = y;
+			// update cell number
+			tail->mzCellNum = cellNum;
 		}
 		else {
 			// The list is empty, create a new node and point head/tail at it
@@ -62,24 +232,25 @@ public:
 			// update x,y of the new node
 			head->nodeX = x;
 			head->nodeY = y;
+			head->mzCellNum = cellNum;
 		}
 	}
 
 	// Updates the contents of node x,y with passed data.
 	// All except 'X' are updated in element 1,1.  X causes all
 	// elements to be updated
-	void updateDLNode(char data, int x, int y){
+	void updateDLNode(char data, int x, int y) {
 		DLNode *temp = head;
-		
+
 		// Iterate through the list
-		while(temp){
+		while (temp) {
 			// Find the node with the passed x,y
-			if(temp->nodeX == x && temp->nodeY == y){
+			if (temp->nodeX == x && temp->nodeY == y) {
 				// Update the node based on passed data.(all X or 1,1 element)
-				if(data !='X') temp->cell[1][1] = data;
-				else{
-					for(int i=0;i<3;i++){
-						for(int j=0;j<3;j++){
+				if (data != 'X') temp->cell[1][1] = data;
+				else {
+					for (int i = 0; i < 3; i++) {
+						for (int j = 0; j < 3; j++) {
 							temp->cell[i][j] = data;
 						}
 					}
@@ -89,10 +260,102 @@ public:
 		}
 	}
 
+	void updateVisited(bool visit, int x, int y) {
+		DLNode *temp = head;
+
+		// Iterate through the list
+		while (temp) {
+			// Find the node with the passed x,y
+			if (temp->nodeX == x && temp->nodeY == y) {
+				// Update the node
+				temp->visited = visit;
+			}
+			temp = temp->next;
+		}
+	}
+
+	void updateBackTrack(bool back, int x, int y) {
+		DLNode *temp = head;
+
+		// Iterate through the list
+		while (temp) {
+			// Find the node with the passed x,y
+			if (temp->nodeX == x && temp->nodeY == y) {
+				// Update the node
+				temp->backTrack = back;
+			}
+			temp = temp->next;
+		}
+	}
+	void updateValidMovementDirections(int rowSize, int colSize) {
+		DLNode *temp = head;
+		DLNode *point = temp;
+		int cellNum = 1;
+
+		// Iterate through the list
+		while (temp) {
+			// assign mzUp to cell above it
+			if (cellNum > rowSize) {
+				for (int i = 1; i < rowSize; i++) {
+					point = point->prev;
+				}
+				temp->mzUp = point;
+				point = temp;
+			}
+			else {
+				temp->mzUp = nullptr;
+				temp->up = false;
+				point = temp;
+			}
+
+			// mzDown
+			if (cellNum <= rowSize * colSize - (rowSize)) {
+				for (int i = 1; i < rowSize + 1; i++) {
+					point = point->next;
+				}
+				temp->mzDown = point;
+				point = temp;
+			}
+			else {
+				temp->mzDown = nullptr;
+				temp->down = false;
+				point = temp;
+			}
+
+			// mzRight
+
+			if ((cellNum % rowSize > 0)) {
+				temp->mzRight = temp->next;
+			}
+			else {
+				temp->mzRight = nullptr;
+				temp->right = false;
+			}
+
+			// mzLeft
+			if (cellNum % rowSize != 1) {
+				temp->mzLeft = temp->prev;;
+			}
+			else {
+				temp->mzLeft = nullptr;
+				temp->left = false;
+			}
+
+			temp = temp->next;
+			cellNum++;
+			//std::cout << std::endl << cellNum;
+		}
+
+	}
+
 	// This function iterates through each row of the arrays 
 	// in the nodes of each row and outputs them one at at time.
 	void printNodes() {
-		
+
+		system("clear");
+
+		std::cout << std::endl << std::endl;
+
 		// Add numerical header to displayed grid
 		std::cout << "   ";
 		for (int i = 0; i < sizeCol; i++) {
@@ -110,10 +373,10 @@ public:
 		DLNode *temp = head;
 
 		// Which node on the row are we at.  Used for condition check
-		rowNodeCount=0;
-		while(rowNodeCount < sizeRow && temp != NULL){
+		rowNodeCount = 0;
+		while (rowNodeCount < sizeRow && temp != NULL) {
 			// iterate the row of the nodes array
-			for(int i=0;i<3;i++){
+			for (int i = 0; i < 3; i++) {
 				std::cout << std::endl;
 
 				// Add row count to first column
@@ -124,14 +387,46 @@ public:
 				else { std::cout << "   "; }
 				// Move temp to the 1st node of the current row
 				temp = rowPtr;
-				colNodeCount=0;
-				while(colNodeCount < sizeCol){
-					for(int j=0;j<3;j++){
-						std::cout << temp->cell[i][j] << " ";
+				colNodeCount = 0;
+				// print the cells
+				while (colNodeCount < sizeCol) {
+					for (int j = 0; j < 3; j++) {
+						if (!temp->visited && !temp->backTrack) {
+							std::cout << temp->cell[i][j] << " ";
+						}
+						else if (temp->visited && !temp->backTrack) {
+							if (i == 1)
+							{
+								if (j == 1)
+									std::cout << "*" << " ";
+								else
+									std::cout << " " << " ";
+							}
+							else {
+								std::cout << temp->cell[i][j] << " ";
+
+							}
+						}
+						else if (temp->visited && temp->backTrack) {
+							if (i == 1) {
+								if (j==1)
+									std::cout << "*" << " ";
+								else
+									std::cout << " " << " ";
+							}
+							else
+							{
+								if (j==1)
+									std::cout << "=" << " ";
+								else
+									std::cout << " " << " ";
+							}
+						}
+
 					}
 					colNodeCount++;
 					// Move temp forward if it is not tail/NULL otherwise
-					if(temp != tail)
+					if (temp != tail)
 						temp = temp->next;
 					else
 						temp = NULL;
@@ -144,7 +439,7 @@ public:
 			rowNodeCount++;
 		}
 	}
-	
+
 	// This function iterates through each row of the arrays 
 	// in the nodes of each row and outputs them one at at time.
 	void writeNodes() {
@@ -168,10 +463,10 @@ public:
 		DLNode *temp = head;
 
 		// Which node on the row are we at.  Used for condition check
-		rowNodeCount=0;
-		while(rowNodeCount < sizeRow && temp != NULL){
+		rowNodeCount = 0;
+		while (rowNodeCount < sizeRow && temp != NULL) {
 			// iterate the row of the nodes array
-			for(int i=0;i<3;i++){
+			for (int i = 0; i < 3; i++) {
 				outFile << std::endl;
 
 				// Add row count to first column
@@ -183,14 +478,14 @@ public:
 
 				// Move temp to the 1st node of the current row
 				temp = rowPtr;
-				colNodeCount=0;
-				while(colNodeCount < sizeCol){
-					for(int j=0;j<3;j++){
+				colNodeCount = 0;
+				while (colNodeCount < sizeCol) {
+					for (int j = 0; j < 3; j++) {
 						outFile << temp->cell[i][j] << " ";
 					}
 					colNodeCount++;
 					// Move temp forward if it is not tail/NULL otherwise
-					if(temp != tail)
+					if (temp != tail)
 						temp = temp->next;
 					else
 						temp = NULL;
@@ -205,11 +500,140 @@ public:
 		outFile.close();
 	}
 
+	// utilize a queue to perform a breadth first search
+	void BreadthFirstSolution() {
+
+	}
+
+	// utilize a stack to perform a depth first search
+	void DepthFirstSolution() {
+		std::stack <int> solutionStack;
+		std::vector <int> stackDisplay;
+		DLNode *temp = head;
+		DLNode *currentNode = head;
+		int gameMode = 1;
+		int deadEnd = 0;
+		this->printNodes();
+
+		// loop until event occurs (0 lose, 2 win)
+		while (gameMode == 1) {
+			// set the current node to visited
+			currentNode->visited = 1;
+
+			// check Up for a node
+			if (currentNode->up) {
+				// check for valid move
+				if (currentNode->mzUp->visited == false && currentNode->mzUp->cell[1][1] != 'X') {
+					// push this cell number to stack
+					solutionStack.push(currentNode->mzCellNum);
+					// push this cell number to vector for stack display
+					stackDisplay.push_back(currentNode->mzCellNum);
+					// move to valid cell
+					currentNode = currentNode->mzUp;
+					// check for finish line
+					if (currentNode->cell[1][1] == 'F') { gameMode = 2; }
+				}
+				// set current cells up flag to false if up is not a good move
+				else { currentNode->up = false; }
+			}
+
+			// check Right for a node <see above for comments>
+			else if (currentNode->right) {
+				if (currentNode->mzRight->visited == false && currentNode->mzRight->cell[1][1] != 'X') {
+					solutionStack.push(currentNode->mzCellNum);
+					stackDisplay.push_back(currentNode->mzCellNum);
+					currentNode = currentNode->mzRight;
+					if (currentNode->cell[1][1] == 'F') { gameMode = 2; }
+				}
+				else { currentNode->right = false; }
+			}
+
+			// check Down for a node <see above for comments>
+			else if (currentNode->down) {
+				if (currentNode->mzDown->visited == false && currentNode->mzDown->cell[1][1] != 'X') {
+					solutionStack.push(currentNode->mzCellNum);
+					stackDisplay.push_back(currentNode->mzCellNum);
+						currentNode = currentNode->mzDown;
+					if (currentNode->cell[1][1] == 'F') { gameMode = 2; }
+				}
+				else { currentNode->down = false; }
+			}
+
+			// check Left for a node <see above for comments>
+			else if(currentNode->left) {
+				if (currentNode->mzLeft->visited == false && currentNode->mzLeft->cell[1][1] != 'X') {
+					solutionStack.push(currentNode->mzCellNum);
+					stackDisplay.push_back(currentNode->mzCellNum);
+					currentNode = currentNode->mzLeft;
+					if (currentNode->cell[1][1] == 'F') { gameMode = 2; }
+				}
+				else { currentNode->left = false; }
+			}
+
+			// if no valid node or move
+			if(!currentNode->up && !currentNode->right && !currentNode->down && !currentNode->left) {
+				// if the stack is empty, there is no solution end game
+				if (solutionStack.empty()) {
+					gameMode = 0;
+				}
+				else {
+					// set current node backTrack flag
+					currentNode->backTrack = true;
+					// get previous cell nubmer from stack
+					int curCell = solutionStack.top();
+					// bool to keep from having to iterate the entire list
+					bool jump = true;
+					// loop until find cell num in the list and set it as current node
+					// pretty much, just go to the top node of the list in the stack
+					while (jump) {
+						if (temp->mzCellNum == curCell) {
+							currentNode = temp;
+							jump = false;
+						}
+						temp = temp->next;
+					}
+					// reset temp back to head
+					temp = head;
+					// pop the stack
+					solutionStack.pop();
+					// remove that cell from the stack display
+					stackDisplay.pop_back();
+					// set the current node backtrack since we've moved back a cell.
+					currentNode->backTrack = true;
+				}
+			}
+			std::cout << std::endl << "Solution Path: ";
+			int iter;
+			for (std::vector<int>::const_iterator iter = stackDisplay.begin(); iter != stackDisplay.end(); ++iter)
+				std::cout << *iter << " ";
+			this->printNodes();
+		}
+
+		// if gamemode 0 lose
+		if (gameMode == 0) {
+			std::cout << std::endl << "Game Over.  There is no path through the maze" << std::endl;
+		}
+		// else if gamemode = 2 win
+		else if (gameMode == 2) {
+			std::cout << std::endl << "Game Over.  Path Found!" << std::endl;
+		}
+		else {
+			std::cout << std::endl << "GAME OVER.  Something Broke!" << std::endl;
+		}
+
+	}
+
 	DLNode *head, *tail;
 	int sizeRow, sizeCol;
 };
 
+
+
 int main() {
+	using namespace std;
+
+	cout << "Maximize this window if necessary, then Press Enter to start...";
+	cin.get();
 
 	int rowSize, colSize;
 	int startRow, startCol;
@@ -217,34 +641,37 @@ int main() {
 	int wallRow, wallCol;
 
 	// Open and read size and start/finish positions from inputfile
-	std::ifstream myFile;
+	ifstream myFile;
 	myFile.open("inputFile");
+	if (!myFile) { cout << "Bad File"; }
 	myFile >> colSize >> rowSize;
 	myFile >> startRow >> startCol >> finishRow >> finishCol;
 
 	// error check the file input
-	if(colSize < 0 || rowSize < 0 ||  startCol < 0 || 
-		startCol > colSize ||  startRow < 0 || startRow > rowSize || 
-		finishCol < 0 || finishCol > colSize||  finishRow < 0 || finishRow > rowSize ){
-		std::cout << "There was an error!  Check file contents." 
-			<< "One of the input numbers is out of range." 
-			<< std::endl << "File input checked: Size, Start, and Finish Coordinates." 
-			<< std::endl;
+	if (colSize < 0 || rowSize < 0 || startCol < 0 ||
+		startCol > colSize || startRow < 0 || startRow > rowSize ||
+		finishCol < 0 || finishCol > colSize || finishRow < 0 || finishRow > rowSize) {
+		cout << "There was an error!  Check file contents."
+			<< "One of the input numbers is out of range."
+			<< endl << "File input checked: Size, Start, and Finish Coordinates."
+			<< endl;
 		return 1;
 	}
 
 	// Create a Doubly Linked List with default content: "."'s
-	DoublyLinkedList* maze = new DoublyLinkedList(rowSize,colSize);
-	for (int i = 0; i <  rowSize; i++){
-		for(int j = 0; j<colSize; j++){
-			maze->addDLNode('.',i,j);
+	DoublyLinkedList* maze = new DoublyLinkedList(rowSize, colSize);
+	int cellNum = 1;
+	for (int i = 0; i < rowSize; i++) {
+		for (int j = 0; j < colSize; j++) {
+			maze->addDLNode(' ', i, j, cellNum);
+			cellNum++;
 		}
 	}
 
 	// update the start and finish coordinages in the list
 	// passes the node coordinates(on a grid) to the function to find the node
-	maze->updateDLNode('S',startRow,startCol);
-	maze->updateDLNode('F',finishRow,finishCol);
+	maze->updateDLNode('S', startRow, startCol);
+	maze->updateDLNode('F', finishRow, finishCol);
 
 
 
@@ -253,24 +680,26 @@ int main() {
 		myFile >> wallCol >> wallRow;
 
 		// error check the file input
-		if (wallCol < 0 || wallCol > colSize || wallRow < 0 || wallRow > rowSize){
-			std::cout << "There was an error!  Check file contents." 
-				<< "One of the input numbers is out of range." 
-				<< std::endl << "File input checked: Wall Cordinates." 
-				<< std::endl;
+		if (wallCol < 0 || wallCol > colSize || wallRow < 0 || wallRow > rowSize) {
+			cout << "There was an error!  Check file contents."
+				<< "One of the input numbers is out of range."
+				<< endl << "File input checked: Wall Cordinates."
+				<< endl;
 			return 1;
 		}
-		maze->updateDLNode('X',wallRow,wallCol);
+		maze->updateDLNode('X', wallRow, wallCol);
 	}
 	myFile.close();
 
-	// print and write to file.
-	maze->printNodes();
-	maze->writeNodes();
+	// update the lists valid movements per cell
+	maze->updateValidMovementDirections(colSize, rowSize);
+
+	// run stack solver (Depth First)
+	maze->DepthFirstSolution();
 
 	// Press a key
-	std::cout << std::endl << "Press <Enter> to continue..." << std::endl;
-	std::cin.get();
-	
+	cout << endl << "Press <Enter> to continue..." << endl;
+	cin.get();
+
 	return 0;
 }
